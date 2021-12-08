da8c20c059b56424f7eaac63c2048b12bd8b227e
Florian Liebhart <florian.liebhart@volkswagen.de>
2021-12-08 19:19:42 +0100

    fix merge conflict

--- src/domain/cocktails.ts
+++ src/domain/cocktails.ts
@@ -5,7 +5,7 @@ import whiskySourSrc from './images/whiskysour.png';
 
 
 const whiskySour = {
-    name: "whisky sour",
+    name: "Whisky Sour",
     picture: whiskySourSrc,
     ingredients: [
         "whisky 6cl",
@@ -27,7 +27,30 @@ const whiskySour = {
 };
 
 
-const cocktails = [whiskySour];
+const negroni = {
+    name: "Negroni",
+    picture: whiskySourSrc,
+    ingredients: [
+        "gim 6cl",
+        "...",
+    ],
+    preparationSteps: [
+        DRY_SHAKE,
+        WET_SHAKE,
+        FINE_STRAIN
+    ],
+    garnishes: [
+        "orange",
+        "...",
+    ],
+    glasses: [
+        NICKANDNORA_GLASS,
+        LOWBALL_GLASS
+    ]
+};
+
+
+const cocktails = [whiskySour, negroni];
 
 
 export default cocktails;
